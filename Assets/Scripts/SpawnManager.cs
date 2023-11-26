d2d9a77b7defb7bad894e21ea4076992beb2e113
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-11-26 17:15:42 +0000

    SpawnRandomEnemy method written

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,11 +1,14 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
- 
+    private float spawnRangeX = 0;
+    private float spawnPosZ = 9;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +19,20 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Spawn slug lawnmower monster enemies
+        //Spawn slug lawnmower monster enemies on pressing S key
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
-
-            Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 5), enemyPrefabs[enemyIndex].transform.rotation);
+            SpawnRandomEnemy();
         }
+    }
 
-      
+    void SpawnRandomEnemy()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeX, spawnRangeX + 3), 0, spawnPosZ);
+
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+
+
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
     }
 }
